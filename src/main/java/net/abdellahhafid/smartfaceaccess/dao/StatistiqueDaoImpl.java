473edab3a916d6d3244baf37ebbc50abb1ff2273
HafidElmoudden <abdulhafid858@gmail.com>
2025-01-16 21:30:23 +0000

    Added stat_date in statistique table, and trigger for updating statistique for each day.

--- src/main/java/net/abdellahhafid/smartfaceaccess/dao/StatistiqueDaoImpl.java
+++ src/main/java/net/abdellahhafid/smartfaceaccess/dao/StatistiqueDaoImpl.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StatistiqueDaoImpl implements StatistiqueDao {
-
     @Override
     public Statistique findById(Integer id) {
         Connection connection = SingletonConnectionDB.getConnection();
@@ -19,6 +18,7 @@ public class StatistiqueDaoImpl implements StatistiqueDao {
             if (resultSet.next()) {
                 statistique = new Statistique();
                 statistique.setId(resultSet.getInt("id"));
+                statistique.setStatDate(resultSet.getDate("stat_date").toString());
                 statistique.setTotalAttempts(resultSet.getInt("total_attempts"));
                 statistique.setSuccessfulAttempts(resultSet.getInt("successful_attempts"));
                 statistique.setFailedAttempts(resultSet.getInt("failed_attempts"));
@@ -33,10 +33,11 @@ public class StatistiqueDaoImpl implements StatistiqueDao {
     public void save(Statistique statistique) {
         Connection connection = SingletonConnectionDB.getConnection();
         try {
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO statistiques (total_attempts, successful_attempts, failed_attempts) VALUES (?, ?, ?)");
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO statistiques (total_attempts, successful_attempts, failed_attempts, stat_date) VALUES (?, ?, ?, ?)");
             statement.setInt(1, statistique.getTotalAttempts());
             statement.setInt(2, statistique.getSuccessfulAttempts());
             statement.setInt(3, statistique.getFailedAttempts());
+            statement.setDate(4, Date.valueOf(statistique.getStatDate()));
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -47,11 +48,12 @@ public class StatistiqueDaoImpl implements StatistiqueDao {
     public void update(Statistique statistique) {
         Connection connection = SingletonConnectionDB.getConnection();
         try {
-            PreparedStatement statement = connection.prepareStatement("UPDATE statistiques SET total_attempts = ?, successful_attempts = ?, failed_attempts = ? WHERE id = ?");
+            PreparedStatement statement = connection.prepareStatement("UPDATE statistiques SET total_attempts = ?, successful_attempts = ?, failed_attempts = ?, stat_date = ? WHERE id = ?");
             statement.setInt(1, statistique.getTotalAttempts());
             statement.setInt(2, statistique.getSuccessfulAttempts());
             statement.setInt(3, statistique.getFailedAttempts());
             statement.setInt(4, statistique.getId());
+            statement.setDate(5, Date.valueOf(statistique.getStatDate()));
             statement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -82,6 +84,7 @@ public class StatistiqueDaoImpl implements StatistiqueDao {
                 statistique.setTotalAttempts(resultSet.getInt("total_attempts"));
                 statistique.setSuccessfulAttempts(resultSet.getInt("successful_attempts"));
                 statistique.setFailedAttempts(resultSet.getInt("failed_attempts"));
+                statistique.setStatDate(resultSet.getDate("stat_date").toString());
                 statistiques.add(statistique);
             }
         } catch (SQLException e) {
